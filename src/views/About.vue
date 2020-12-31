34961b51fbad0e7fe58470e90f8f1401fc1dcf4c
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-30 22:08:47 -0600

    add navbar

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+      molestias architecto ab officia dolor eligendi magni cum cumque labore
+      enim, ratione sunt modi, maxime at pariatur voluptas consectetur commodi
+      praesentium.
+    </p>
   </div>
 </template>
